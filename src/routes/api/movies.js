e06f2ea35dba37b8db69687d6213e93fe689df21
IvanStacul <stacul.ivan98@gmail.com>
2021-10-24 22:33:19 -0300

    filter movies

--- src/routes/api/movies.js
+++ src/routes/api/movies.js
@@ -1,12 +1,38 @@
 const router = require("express").Router();
-
+const { Sequelize } = require("sequelize");
+const Op = Sequelize.Op;
 const { Movie } = require("../../database/config/tables");
 
 router.get("/", async (req, res) => {
-    const movies = await Movie.findAll({
-        attributes: ["title", "image", "releaseDate"],
-    });
-    res.json(movies);
+    let movies;
+    let query = req.query;
+
+    if (query.title) {
+        let title = query.title;
+
+        movies = await Movie.findAll({
+            where: {
+                title: {
+                    [Op.like]: `%${title}%`,
+                },
+            },
+        });
+    } else if (query.genre) {
+        movies = await Movie.findAll({
+            where: {
+                GenreId: query.genre,
+            },
+        });
+    } else if (query.order) {
+        movies = await Movie.findAll({
+            order: [["releaseDate", query.order]],
+        });
+    } else {
+        movies = await Movie.findAll({
+            attributes: ["title", "image", "releaseDate"],
+        });
+    }
+    res.status(200).json(movies);
 });
 
 router.post("/", async (req, res) => {
@@ -18,14 +44,14 @@ router.put("/:movieId", async (req, res) => {
     await Movie.update(req.body, {
         where: { id: req.params.movieId },
     });
-    res.json({success: "Movie updated."});
+    res.json({ success: "Movie updated." });
 });
 
 router.delete("/:movieId", async (req, res) => {
     await Movie.destroy({
         where: { id: req.params.movieId },
     });
-    res.json({success: "Movie deleted."});
+    res.json({ success: "Movie deleted." });
 });
 
 module.exports = router;

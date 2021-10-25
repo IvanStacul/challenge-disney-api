2fdc685ce944f6dfd1269946af6f4f4c6c47e45c
IvanStacul <stacul.ivan98@gmail.com>
2021-10-24 22:18:52 -0300

    filter characters

--- src/routes/api/characters.js
+++ src/routes/api/characters.js
@@ -1,12 +1,65 @@
 const router = require("express").Router();
-
+const { Sequelize } = require("sequelize");
+const Op = Sequelize.Op;
 const { Character } = require("../../database/config/tables");
 
 router.get("/", async (req, res) => {
-    const movies = await Character.findAll({
-        attributes: ["name", "image"],
-    });
-    res.json(movies);
+    let query = req.query;
+    let response;
+
+    if (query.name) {
+        response = await Character.findAll({
+            where: {
+                name: {
+                    [Op.like]: `%${query.name}%`,
+                },
+            },
+            attributes: ["name", "image"],
+        });
+    } else if (query.age) {
+        responseCharacter = await Character.findAll({
+            where: {
+                age: query.age,
+            },
+            attributes: ["name", "image"],
+        });
+    } else if (query.weight) {
+        response = await Character.findAll({
+            where: {
+                weight: query.weight,
+            },
+            attributes: ["name", "image"],
+        });
+    } else if (query.movies) {
+        responseCharacter = await Character.findAll({
+            attributes: ["name", "image"],
+            include: [
+                {
+                    model: Movie,
+                    through: {
+                        attributes: [],
+                    },
+                    where: {
+                        id: query.movies,
+                    },
+                    attributes: ["image", "title"],
+
+                    include: [
+                        {
+                            model: Genre,
+                            attributes: ["name"],
+                        },
+                    ],
+                },
+            ],
+        });
+    } else {
+        response = await Character.findAll({
+            attributes: ["name", "image"],
+        });
+    }
+
+    res.status(200).json(response);
 });
 
 router.post("/", async (req, res) => {
